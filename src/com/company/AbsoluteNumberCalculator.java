9b3eb909d2bd7a77207a8f943fdb817d25b45afc
Hungyuuki <hungyuuki777@gmail.com>
2022-01-14 16:10:18 +0700

    1

---
+++ src/com/company/AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class AbsoluteNumberCalculator {
+
+        public static int findAbsoluteNumber(int number){
+            return Math.abs(number);
+        }
+    }
+
---
+++ test/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,21 @@
+import com.company.AbsoluteNumberCalculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class AbsoluteNumberCalculatorTest {
+    @Test
+    public void AbsNumberTestNegative2() {
+        int expectedValue = 2;
+        int actualValue = AbsoluteNumberCalculator.findAbsoluteNumber(-2);
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void AbsNumberTestNegative1() {
+        int expectedValue = -1;
+        int actualValue = AbsoluteNumberCalculator.findAbsoluteNumber(-1);
+        assertEquals(expectedValue, actualValue);
+    }
+}
